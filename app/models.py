ed38488b38dde72d2b6ab0165edd62a380bd5cdb
Betul Yazici <betulyzc.066@gmail.com>
2025-04-07 23:08:37 +0300

    feat: define Project and Issue models with relationships and enums

---
+++ app/models.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column,Integer,String,ForeignKey,DateTime,Enum,Text
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from database import Base
+import enum #It is used to determine some values ​​as constant.
+
+
+#The fixed values ​​(enum) that the status and priority fields can take are defined.
+class IssueStatus(str, enum.Enum):
+    open="open"
+    in_progress="in_progress"
+    closed="closed"
+
+class IssuePriority(str,enum.Enum):
+    low="low"
+    medium="medium"
+    high="high"
+
+class Project(Base):
+    __tablename__="Projects"
+
+    id = Column( Integer, primary_key=True,index=True )
+    name=Column( String, nullable=False)
+    description=Column(Text)
+
+    issues=relationship("Issue", back_populates="project", cascade="all, delete")
+
+class Issue(Base):
+    __tablename__ = "Issues"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    description = Column(Text)
+    status = Column(Enum(IssueStatus), default=IssueStatus.open)
+    priority = Column(Enum(IssuePriority), default=IssuePriority.medium)
+    assignee = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    project_id = Column(Integer, ForeignKey("projects.id"))
+    project = relationship("Project", back_populates="issues")
